d5a6ca200820a668b1546476ea7a2b506d9dcc22
viniciusfranca019 <viniciusfrancas019@gmail.com>
2021-02-25 22:32:14 -0300

    parte 1

---
+++ bloco_4/exe1.js
@@ -0,0 +1,10 @@
+let a b n;
+let subtracao = a-b ;
+let adicao = a+b ;
+let multi = a*b ;
+let divi - a/b ;
+let modu = Math.abs(n) ;
+console.log("a subtração é: "+subtracao) ;
+console.log("a adição é: "+adicao);
+n = a + b
+console.log("o modulo  é: "+modu)
