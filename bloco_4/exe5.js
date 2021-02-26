b5e40449061cf7bd58d74c738b2e251b21b7c08d
viniciusfranca019 <viniciusfrancas019@gmail.com>
2021-02-25 23:05:23 -0300

    exercise 5

---
+++ bloco_4/exe5.js
@@ -0,0 +1,21 @@
+let a,b,c ;
+a = 180 ;
+b = 0 ;
+c = 0 ;
+if((a+b+c)== 180) {
+  if(a < 180){
+      if(b<180){
+          if(c<180) {
+              console.log('é um triângulo')
+            }else {
+                console.log('não é um triângulo')
+            }
+          } else {
+            console.log('não é um triângulo')  
+          }
+  }else {
+    console.log('não é um triângulo')  
+  }
+}else {
+    console.log('não é um triângulo')  
+}
