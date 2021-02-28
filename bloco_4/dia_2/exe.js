f702b4d4e4dbb9d7c935c881fa530ba95166bd43
viniciusfranca019 <viniciusfrancas019@gmail.com>
2021-02-28 12:43:20 -0300

    ex 5

--- bloco_4/dia_2/exe.js
+++ bloco_4/dia_2/exe.js
@@ -26,3 +26,12 @@ if(mediaArit > 20) {
 }
 
 // ex 5
+
+let maiorNumber = numbers[0];
+
+for(let i = 0; i < numbers.length - 1;i += 1) {
+  if(numbers[i] > maiorNumber) {
+    maiorNumber = numbers[i];
+  }
+}
+console.log('o maior numero é '+maiorNumber)
