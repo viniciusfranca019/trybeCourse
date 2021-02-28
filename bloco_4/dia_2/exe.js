3388f5cea2de37ceed77e09d076b8bf892958b68
viniciusfranca019 <viniciusfrancas019@gmail.com>
2021-02-28 12:25:59 -0300

    ex2,ex3 and sorry for the big commit

--- bloco_4/dia_2/exe.js
+++ bloco_4/dia_2/exe.js
@@ -1,5 +1,20 @@
+// ex 1
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
-for(let i = 0; i < numbers.length - 1;i+=1){
+// for(let i = 0; i < numbers.length - 1;i+=1){
   console.log(numbers[i]);
 }
+
+// ex2
+let sum = 0;
+for(let i = 0; i < numbers.length - 1;i+=1){
+  sum += numbers[i];
+}
+console.log('a soma é '+sum)
+
+// ex 3
+
+let mediaArit = sum/numbers.length;
+console.log('a média aritmetica é '+mediaArit);
+
+// ex 4
