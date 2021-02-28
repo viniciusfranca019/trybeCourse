34518a0187a2321c39b1a2ba7daef5590e6a837f
viniciusfranca019 <viniciusfrancas019@gmail.com>
2021-02-28 13:01:10 -0300

    ex 6 & for codition corrected

--- bloco_4/dia_2/exe.js
+++ bloco_4/dia_2/exe.js
@@ -1,13 +1,13 @@
 // ex 1
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
-for(let i = 0; i < numbers.length - 1;i+=1){
+for(let i = 0; i < numbers.length ;i+=1){
   console.log(numbers[i]);
 }
 
 // ex2
 let sum = 0;
-for(let i = 0; i < numbers.length - 1;i+=1){
+for(let i = 0; i < numbers.length ;i+=1){
   sum += numbers[i];
 }
 console.log('a soma é '+sum)
@@ -29,9 +29,35 @@ if(mediaArit > 20) {
 
 let maiorNumber = numbers[0];
 
-for(let i = 0; i < numbers.length - 1;i += 1) {
+for(let i = 0; i < numbers.length ;i += 1) {
   if(numbers[i] > maiorNumber) {
     maiorNumber = numbers[i];
   }
 }
 console.log('o maior numero é '+maiorNumber)
+
+// ex 6
+
+let oddNumber = [];
+
+for(let i = 0; i < numbers.length ;i += 1) {
+  if(numbers[i] % 2 !== 0) {
+    oddNumber.push(numbers[i]);
+  }
+}
+if(oddNumber.length == 0){
+  console.log('não há número impar');
+} else {
+  console.log('os numeros impares são '+oddNumber)
+}
+
+// ex 7
+
+let menorNumber = numbers[0]
+
+for(let i = 0; i < numbers.length ;i += 1) {
+  if(numbers[i] < menorNumber) {
+    menorNumber = numbers[i];
+  }
+}
+console.log('o menor numero é '+menorNumber);
