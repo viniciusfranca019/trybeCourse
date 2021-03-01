9a25f54ca965cfbdf23e72a53d34900707e32726
viniciusfranca019 <viniciusfrancas019@gmail.com>
2021-03-01 20:53:34 -0300

    conseguir babys, partu bonus

--- bloco_4/dia_3/ex4.js
+++ bloco_4/dia_3/ex4.js
@@ -1,18 +1,31 @@
-let number = 5;
+let number = 6;
 let linha = [];
 for(let i = 0;i < number;i+=1) {
   linha.push(' ');
 }
-if(number%2 !==0) {
+if(number % 2 !== 0) {
   for(let i = 0;i < ((number+1)/2);i+=1) {
-    let linhaP = ''
+    let linhaP = '';
     linha[((number+1)/2)-1+i] = '*';
     linha[((number+1)/2)-1-i] = '*';
     for(let i = 0;i < number;i+=1) {
-      linhaP += linha[i]
+      linhaP += linha[i];
     }
-    console.log(linhaP)
+    console.log(linhaP);
     linhaP = '';
-    console.log(linhaP)
+    console.log(linhaP);
+  }
+} else {
+  for(let i = 0;i < ((number)/2);i+=1) {
+    let linhaP = '';
+    linha[((number)/2)-1-i] = '*';
+    linha[((number)/2)+i] = '*';
+    for(let i = 0;i < number;i+=1) {
+      linhaP += linha[i];
+    }
+    console.log(linhaP);
+    linhaP = '';
+    console.log(linhaP);
   }
 }
+
