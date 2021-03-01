89c99cdf1fcc1aa65bd6e04bf81d676d864d9388
viniciusfranca019 <viniciusfrancas019@gmail.com>
2021-03-01 19:50:40 -0300

    att

---
+++ bloco_4/dia_2/ex2.js
@@ -0,0 +1,16 @@
+// ex 8
+let numbers = [];
+
+for(let i = 0;i < 25 ;i += 1) {
+  numbers.push(i+1);
+  console.log(numbers[i]);
+}
+
+// ex 9
+
+let divPor2 = 0;
+
+for(let i = 0;i < 25 ;i += 1) {
+  divPor2 = numbers[i]/2
+  console.log(divPor2)
+}
