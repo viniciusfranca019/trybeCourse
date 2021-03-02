7ca5d5dd491180cb9942cae5d2d08a27548a6a44
viniciusfranca019 <viniciusfrancas019@gmail.com>
2021-03-01 22:39:27 -0300

    ex5

---
+++ bloco_4/dia_3/ex5.js
@@ -0,0 +1,29 @@
+let number = 5;
+let linha = [];
+for(let i = 0;i < number;i+=1) {
+  linha.push(' ');
+}
+
+if(number % 2 !== 0) {
+  for(let i = 0;i < ((number+1)/2);i+=1) {
+    let linhaP = '';
+    let resetLinha = linha;
+    linha[((number+1)/2)-1+i] = '*';
+    linha[((number+1)/2)-1-i] = '*';
+    for(let i = 0;i < number;i+=1) {
+      linhaP += linha[i];
+    }
+    console.log(linhaP);
+		linha = resetLinha;
+  }
+} else {
+  for(let i = 0;i < ((number)/2);i+=1) {
+    let linhaP = '';
+    linha[((number)/2)-1-i] = '*';
+    linha[((number)/2)+i] = '*';
+    for(let i = 0;i < number;i+=1) {
+      linhaP += linha[i];
+    }
+    console.log(linhaP);
+  }
+}
