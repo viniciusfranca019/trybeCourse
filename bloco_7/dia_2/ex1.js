b0f3981ba0be1ecc39e42cc835e1efd213e0e6ac
viniciusfranca019 <viniciusfrancas019@gmail.com>
2021-03-31 05:23:05 -0300

    att rep

---
+++ bloco_7/dia_2/ex1.js
@@ -0,0 +1,50 @@
+const order = {
+    name: 'Rafael Andrade',
+    phoneNumber: '11987631416',
+    address: {
+      street: 'Rua das Flores',
+      number: '389',
+      apartment: '701',
+    },
+    order: {
+      pizza: {
+        margherita: {
+          amount: 1,
+          price: 25,
+        },
+        pepperoni: {
+          amount: 1,
+          price: 20,
+        }
+      },
+      drinks: {
+        coke: {
+          type: 'Coca-Cola Zero',
+          price: 10,
+          amount: 1,
+        }
+      },
+      delivery: {
+        deliveryPerson: 'Ana Silveira',
+        price: 5,
+      }
+    },
+    payment: {
+      total: 60,
+    },
+  };
+  
+  const customerInfo = (order) => {
+    const deliveryPerson = order.order.delivery.deliveryPerson;
+    const msg = `Olá , ${deliveryPerson}`
+  
+  }
+  
+  customerInfo(order);
+  
+  const orderModifier = (order) => {
+    // Adicione abaixo as informações necessárias.
+  
+  }
+  
+  orderModifier(order);
