53442e88cb295b878b4540ff29f9c08a584c5f16
viniciusfranca019 <viniciusfrancas019@gmail.com>
2021-04-07 19:09:38 -0300

    att repo com atividades dia 8.5

---
+++ HOF_exercises/dia_5/propostos/exe5.js
@@ -0,0 +1,11 @@
+const assert = require('assert');
+
+const myList = [1, 2, 3];
+
+const swap = (arr) => arr.map((element,i,ray) => ray[ray.length - 1 - i]);
+
+const swappedList = swap(myList);
+console.log(swappedList)
+assert.strictEqual(swappedList[0], 3);
+assert.strictEqual(swappedList[1], 2);
+assert.strictEqual(swappedList[2], 1);
