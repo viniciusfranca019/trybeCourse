542b77476ec6d296afa88a28485c585360911b83
viniciusfranca019 <viniciusfrancas019@gmail.com>
2021-04-12 05:49:08 -0300

    att repo dia 9.1

--- HOF_exercises/dia_5/propostos/exe5.js
+++ HOF_exercises/dia_5/propostos/exe5.js
@@ -2,7 +2,7 @@ const assert = require('assert');
 
 const myList = [1, 2, 3];
 
-const swap = (arr) => arr.map((element,i,ray) => ray[ray.length - 1 - i]);
+const swap = (arr) => arr.map((element, i, ray) => ray[ray.length - 1 - i]);
 
 const swappedList = swap(myList);
 console.log(swappedList)
